48b4112a1bf41a614c2278f0caf0ab1355a90e7e
Yuva Kunaal <bhavikunaal@gmail.com>
2024-07-22 10:15:49 +0530

    m22

--- notes-app/app.py
+++ notes-app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template, redirect, request
 from db import noteTablesCreate
-from db import Note,readAllNotes
+from db import Note
+from db import readAllNotes
 from db import readNoteById
 from db import updateNote
 from db import createNote
@@ -13,14 +14,14 @@ app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "GET":
-        return render_template('index.html')
+        return render_template('index.html',menu='Search')
     elif request.method == "POST":
         title = request.form["title"]
         notes_text = request.form["notes"]
         notes = searchNote(title,notes_text)
         for i in range(len(notes)):
             notes[i].SNO = i+1
-        return render_template('list.html',notes=notes)
+        return render_template('list.html',notes=notes,menu='All')
 
 @app.route("/create",methods=["GET","POST"])
 def create_notes():
@@ -31,25 +32,25 @@ def create_notes():
         createNote(note)
         return redirect('/list')
     elif request.method == "GET":
-        return render_template('create.html')
+        return render_template('create.html',menu='New')
 
 @app.route("/list",methods=["GET"])
 def list_notes():
     notes = readAllNotes()
     for i in range(len(notes)):
         notes[i].SNO = i+1
-    return render_template('list.html',notes=notes)
+    return render_template('list.html',notes=notes,menu='All')
 
-@app.route("/view/<id>",methods=["GET"])
+@app.route("/list/view/<id>",methods=["GET"])
 def view_notes(id):
     note = readNoteById(id)
-    return render_template('view.html',note=note)
+    return render_template('view.html',note=note,menu='All')
 
-@app.route("/edit/<id>",methods=["GET","POST"])
+@app.route("/list/edit/<id>",methods=["GET","POST"])
 def edit_notes(id):
     note = readNoteById(id)
     if request.method == "GET":
-        return render_template('edit.html',note=note)
+        return render_template('edit.html',note=note,menu='All')
     elif request.method == "POST":
         note.title = request.form["title"]
         note.notes = request.form["notes"]
